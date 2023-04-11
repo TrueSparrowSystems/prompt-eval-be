78e7d775c9acacbab017e1e1b478ac5bf20960a6
Shraddha Falane <72125392+shraddhafalane@users.noreply.github.com>
2023-04-11 09:10:52 -0700

    Graphql boilerplate (#1)
    Graphql boilerplate: Query, Query by id, Query by field, Mutation: create, update and Pagination

--- graphQL/schema.py
+++ graphQL/schema.py
@@ -1,19 +1,38 @@
 import graphene
-from graphene.relay import Node
+from graphene.relay import Node, Connection
 from graphene_mongo.fields import MongoengineConnectionField
-from .mutations import CreateExperimentMutation
+from .mutations import CreateExperimentMutation, CreateExampleMutation, UpdateExperimentMutation
 from .models import Experiment
-from .types import ExperimentType
-
+from .types import ExperimentType, ExampleType , ExperimentConnection
 
 class Mutations(graphene.ObjectType):
     create_experiment = CreateExperimentMutation.Field()
-
+    create_example = CreateExampleMutation.Field()
+    update_experiment = UpdateExperimentMutation.Field()
 class Query(graphene.ObjectType):
     node = Node.Field()
-    experiment_list = graphene.List(ExperimentType)
+    experiment_list = graphene.List(ExperimentType, name=graphene.String())
+    experiment_list_by_id = graphene.Field(ExperimentType, documentId=graphene.String(required=True))
+    experiments_by_pagination = MongoengineConnectionField(ExperimentType)
 
-    def resolve_experiment_list(root, info):
-        return Experiment.objects.all()
+    def resolve_experiment_list(root, info, name=None):
+        if name:
+            return Experiment.objects.filter(name=name)            
+        else:
+            return Experiment.objects.all()
+            
+    def resolve_experiment_list_by_id(self, info, documentId=graphene.String(required=True)):
+        try:
+            return Experiment.objects.get(_id=documentId)
+        except Experiment.DoesNotExist:
+            return None
+    
+    def resolve_experiments_by_pagination(self, info, **kwargs):
+        try:
+            queryset = Experiment.objects.order_by('id')
+            return queryset
+        except queryset.DoesNotExist:
+            return None
 
-schema = graphene.Schema(query=Query, mutation=Mutations, types=[ExperimentType])
+    
+schema = graphene.Schema(query=Query, mutation=Mutations, types=[ExperimentType, ExampleType, ExperimentConnection])
--- graphQL/types.py
+++ graphQL/types.py
@@ -1,9 +1,20 @@
 from graphene import relay
 from graphene_mongo import MongoengineObjectType
-from .models import Experiment
-
+from .models import Experiment, Example
 
 class ExperimentType(MongoengineObjectType):
     class Meta:
         model = Experiment
         interfaces = (relay.Node,)
+
+class ExampleType(MongoengineObjectType):
+    class Meta:
+        model = Example
+        interfaces = (relay.Node,)
+
+class ExperimentConnection(relay.Connection):
+     class Meta:
+        node = ExperimentType
+
+
+
