0cdbd5290ccdfee6016415342800c86319f88eaf
Yash Shah <73335063+yash-ttxt@users.noreply.github.com>
2023-04-11 13:49:55 +0530

    Revert "init (#1)"

--- graphQL/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
-
-urlpatterns = [
-    path(
-        "", csrf_exempt(GraphQLView.as_view(graphiql=True)), name="graphql-query"
-    )
-]
