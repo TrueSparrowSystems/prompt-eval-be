eeaea63228504d3d0fe90e3981e4ef8c499e69d6
Yagnik Thummar <64009176+yagnik159@users.noreply.github.com>
2023-04-13 13:56:34 +0530

    pagination (#13)

--- graphQL/types.py
+++ graphQL/types.py
@@ -1,29 +1,34 @@
-from graphene import relay
+from graphene import relay,String,Int, List, ObjectType
 from graphene_mongo import MongoengineObjectType
 from .models import Experiment, Example, PromptTemplate
 
-
+class DynamicVarsType(ObjectType):
+    key = String(required=True)
+    value = String(required=True)
 
 class ExperimentType(MongoengineObjectType):
     class Meta:
         model = Experiment
-        interfaces = (relay.Node,)
+    
+    name = String(required=True)
+    description = String(required=True)
+    dyanamic_vars = List(DynamicVarsType)
+    created_at = Int()
+    updated_at = Int()
+
+class ExperimentPaginationType(ObjectType):
+    total_count = Int()
+    items = List(ExperimentType)
 
 class ExampleType(MongoengineObjectType):
     class Meta:
         model = Example
         interfaces = (relay.Node,)
 
-class ExperimentConnection(relay.Connection):
-     class Meta:
-        node = ExperimentType
+
 
 class PromptTemplateType(MongoengineObjectType):
     
     class Meta:
         model = PromptTemplate
         interfaces = (relay.Node,)
-
-
-
-
