da4ae6fbda2b0324c7873a373b889c30bf7f0f68
Yagnik Thummar <yagnikthummar0204@gmail.com>
2023-04-14 11:24:56 +0530

    create new experiments and updation

--- graphQL/types.py
+++ graphQL/types.py
@@ -1,18 +1,15 @@
-from graphene import relay,String,Int, List, ObjectType
+from graphene import relay,String,Int, List, ObjectType, ID, Float, InputObjectType
 from graphene_mongo import MongoengineObjectType
 from .models import Experiment, Example, PromptTemplate
 
-class DynamicVarsType(ObjectType):
-    key = String(required=True)
-    value = String(required=True)
 
-class ExperimentType(MongoengineObjectType):
-    class Meta:
-        model = Experiment
-    
-    name = String(required=True)
-    description = String(required=True)
-    dyanamic_vars = List(DynamicVarsType)
+
+class ExperimentType(ObjectType):
+
+    id = ID()
+    name = String()
+    description = String()
+    dynamic_vars = List(String)
     created_at = Int()
     updated_at = Int()
 
@@ -25,10 +22,32 @@ class ExampleType(MongoengineObjectType):
         model = Example
         interfaces = (relay.Node,)
 
+class ReportType(ObjectType):
+    id = ID()
+    model = String(required=True)
+    eval = String(required=True)
+    accuracy = Float()
+    prompt_template_id = ID(required=True)
+    evaluation_report_data = String()
+    run_id = Int()
+    status = String()
+    completed_at = Int()
+    created_at = Int()
+    updated_at = Int()
 
+class InputConversationType(InputObjectType):
+    role = String()
+    content = String()
 
-class PromptTemplateType(MongoengineObjectType):
-    
-    class Meta:
-        model = PromptTemplate
-        interfaces = (relay.Node,)
+class OutputConversationType(ObjectType):
+    role = String()
+    content = String()
+
+class PromptTemplateType(ObjectType):
+    id = ID()
+    name = String(required=True)
+    description = String()
+    conversation = List(OutputConversationType)
+    experiment_id = ID(required=True)
+    created_at = Int()
+    updated_at = Int()
