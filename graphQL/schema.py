7c163e9defe59cf43174d6882172a0d9b8002183
Yagnik Thummar <yagnikthummar0204@gmail.com>
2023-04-18 10:02:30 +0530

    Removed unused code.

--- graphQL/schema.py
+++ graphQL/schema.py
@@ -1,18 +1,14 @@
 import graphene
-from graphene.relay import Node
-from .mutations import  CreateExampleMutation
 from graphQL.graphene_mutations.create_experiment import CreateExperimentMutation
 from graphQL.graphene_mutations.update_experiment import UpdateExperimentMutation
 from graphQL.graphene_mutations.create_prompt_template import CreatePromptTemplateMutation
 from graphQL.graphene_mutations.create_test_cases import CreateTestCasesMutation
-from graphQL.graphene_mutations.update_prompt_template import UpdatePromptTemplateMutation
 from graphQL.db_models.experiment import Experiment
 from graphQL.db_models.prompt_template import PromptTemplate
 from graphQL.db_models.test_case import TestCase
 from graphQL.graphene_types.experiment import ExperimentType, ExperimentPaginationType
 from graphQL.graphene_types.prompt_template import PromptTemplateType, PromptTemplatePaginationType
 from graphQL.graphene_types.test_case import TestCaseType
-from .types import ExampleType
 from graphql import GraphQLError
 
 class Mutations(graphene.ObjectType):
@@ -20,18 +16,12 @@ class Mutations(graphene.ObjectType):
     update_experiment = UpdateExperimentMutation.Field()
     create_prompt_template = CreatePromptTemplateMutation.Field()
     create_test_cases = CreateTestCasesMutation.Field()
-    update_prompt_template = UpdatePromptTemplateMutation.Field()
-    create_example = CreateExampleMutation.Field()
 
     
 class Query(graphene.ObjectType):
-    node = Node.Field()
     experiment_list = graphene.List(ExperimentType)
     prompt_list_by_pagination = graphene.Field(PromptTemplatePaginationType, experimentId=graphene.String(required=True), limit=graphene.Int(required=True), page=graphene.Int(required=True))
     test_cases = graphene.List(TestCaseType, experimentId=graphene.String(required=True))
-    experiment_list_by_id = graphene.Field(ExperimentType, documentId=graphene.String(required=True))
-    experiments_by_pagination = graphene.List(ExperimentType, limit=graphene.Int(required=True), page=graphene.Int())
-    experiments_by_pagination_count = graphene.Field(ExperimentPaginationType, limit=graphene.Int(required=True), page=graphene.Int())
 
     def resolve_experiment_list(root, info): 
         try:
@@ -82,30 +72,8 @@ class Query(graphene.ObjectType):
              }
             )
             return error
-    
-    def resolve_experiment_list_by_id(self, info, documentId=graphene.String(required=True)):
-        try:
-            return Experiment.objects.get(_id=documentId)
-        except Experiment.DoesNotExist:
-            return None
-    
-    def resolve_experiments_by_pagination(self, info, **kwargs):
-        limit = kwargs.get('limit')
-        page = kwargs.get('page', 0)
-        offset = page * limit
-        return Experiment.objects[offset:offset+limit]
-    
-    def resolve_experiments_by_pagination_count(self, info, **kwargs):
-        limit = kwargs.get('limit')
-        page = kwargs.get('page', 0)
-        offset = page * limit
-        
-        total_count = Experiment.objects.count()
-        experiments = Experiment.objects[offset:offset+limit]
-
-        return ExperimentPaginationType(total_count=total_count, items=experiments)
 
     
-schema = graphene.Schema(query=Query, mutation=Mutations, types=[ExperimentType, ExampleType, PromptTemplateType, ExperimentPaginationType])
+schema = graphene.Schema(query=Query, mutation=Mutations, types=[ExperimentType, PromptTemplateType, ExperimentPaginationType])
 
 
