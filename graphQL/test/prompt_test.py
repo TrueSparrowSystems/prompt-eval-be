cd6b54c1c2d1204d958f382c2e050e7ef864472f
Shraddha Falane <falane.shraddha18@gmail.com>
2023-04-19 13:27:26 +0530

    prompt template testcases

--- graphQL/test/prompt_test.py
+++ graphQL/test/prompt_test.py
@@ -2,7 +2,7 @@ import json
 from graphene_django.utils.testing import GraphQLTestCase
 
 # Command to run test cases for experiment
-# $ python manage.py test graphQL.test.prompt_experiment
+# $ python manage.py test graphQL.test.prompt_test
 # Create your tests here.
 class PromptTest(GraphQLTestCase):
     def test_get_prompts_by_experiment_id(self):
@@ -40,34 +40,78 @@ class PromptTest(GraphQLTestCase):
             '''
             mutation {
             createPromptTemplate(
-                experimentId: "643d4e97c865c10095c972e5"
-                name: "Prompt 6"
-                description: "desc of Sixth one"
-                conversation: [{role: "SYSTEM", content: "I'm chat"}, {role: "USER", content: "I know chat"},{role:"ASSISTENT", content:"BYE"}]
-            ) {
-                promptTemplate {
+                promptTemplateData:{
+                    name:"Test Prompt",
+                    description:"This is a test prompt",
+                    experimentId:"643d4e97c865c10095c972e5",
+                    conversation:[{role:"system",content:"newone"}]
+                    }
+                ) {
+                promptTemplate{
                 id
                 name
                 description
-                createdAt
-                updatedAt
-                conversation {
+                conversation{
                     role
                     content
+                    }
+                createdAt
+                updatedAt
                 }
-                }
-            }
+             }
             }
             '''
         )
 
         content = json.loads(response.content)
-        print('################################################\n')
         # This validates the status code and if you get errors
         self.assertResponseNoErrors(response)
-        # self.assertEqual(content['data']['createPromptTemplate']['prompt']['name'], 'Test Prompt')
-        # self.assertEqual(content['data']['createPromptTemplate']['prompt']['description'], 'This is a test prompt')
+        self.assertEqual(content['data']['createPromptTemplate']['promptTemplate']['name'], 'Test Prompt')
+        self.assertEqual(content['data']['createPromptTemplate']['promptTemplate']['description'], 'This is a test prompt')
 
         # Add some more asserts if you like
+        
+    # Write test case for create prompt mutation having length of name greater than 70 characters also compare error dictionary with expcted dictionary
+    def test_create_prompt_mutation_invalid_length(self):
+        response = self.query(
+            '''
+            mutation {
+            createPromptTemplate(
+                promptTemplateData:{
+                    name:"Test Prompt Test Prompt Test Prompt Test Prompt Test Prompt Test Prompt Test Prompt Test Prompt",
+                    description:"This is a test prompt",
+                    experimentId:"643d4e97c865c10095c972e5",
+                    conversation:[{role:"system",content:"newone"}]
+                    }
+                ) {
+                promptTemplate{
+                id
+                name
+                description
+                conversation{
+                    role
+                    content
+                    }
+                createdAt
+                updatedAt
+                }
+             }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        # assert equal error dictionary with expected dictionary
+         # assert equal error dictionary with expected dictionary
+        print('--------------------------######_----------',content)
+        self.assertEqual(content['errors'][0]['message'], 'Invalid length')
+        self.assertEqual(content['errors'][0]['path'][0], 'createPromptTemplate')
+        self.assertEqual(content['errors'][0]['locations'][0]['line'], 3)
+        self.assertEqual(content['errors'][0]['locations'][0]['column'], 13)
+
+        
+        
       
     
