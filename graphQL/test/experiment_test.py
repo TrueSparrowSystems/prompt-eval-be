913f812294d6f2172c4463d7bf8a7e7eb6cc2c38
Shraddha Falane <72125392+shraddhafalane@users.noreply.github.com>
2023-04-19 03:45:57 -0700

    Testcases (#45)
    Test cases for queries and mutations

---
+++ graphQL/test/experiment_test.py
@@ -0,0 +1,174 @@
+import json
+from graphene_django.utils.testing import GraphQLTestCase
+
+# Command to run test cases for experiment
+# $ python manage.py test graphQL.test.experiment_test
+# Create your tests here.
+class ExperimentTest(GraphQLTestCase):
+    def test_get_experiments_query(self):
+        response = self.query(
+            '''
+            query {
+                experimentList {
+                    id
+                    name
+                    description
+                    createdAt
+                    updatedAt
+                    dynamicVars
+                }
+            }
+            '''      
+          )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+      
+        # Add some more asserts if you like
+      
+    def test_create_experiment_mutation(self):
+        response = self.query(
+            '''
+            mutation{
+                createExperiment(experimentData:{name:"Test Experiment",description:"This is a test experiment"}){
+                    experiment {
+                    id
+                    name
+                    description
+                    dynamicVars
+                    createdAt
+                    updatedAt
+                    }
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+        print('################################################\n')
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+        self.assertEqual(content['data']['createExperiment']['experiment']['name'], 'Test Experiment')
+        self.assertEqual(content['data']['createExperiment']['experiment']['description'], 'This is a test experiment')
+
+    
+    def test_create_experiment_mocked_mutations(self):
+      response = self.query(
+            '''
+            mutation {
+                createExperiment(experimentData:{description:"description of 1 "}) {
+                    experiment {
+                        id
+                        name
+                        description
+                        createdAt
+                        updatedAt
+                        dynamicVars
+                    }
+                }
+            }
+            '''
+        )
+    
+      self.assertResponseHasErrors(response)
+      
+    def test_update_experiment_mutation(self):
+        response = self.query(
+            '''
+            mutation {
+                updateExperiment(
+                    updateExperimentData:
+                        {
+                            name:"Test Experiment Update",
+                            id:"6438f6ea0b7e9d05970def5a"
+                        }
+                ) {
+                    experiment{
+                    id
+                    name
+                    description
+                    dynamicVars
+                    createdAt
+                    updatedAt
+                  }
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+
+        # Add some more asserts if you like
+    
+    def test_update_experiment_mocked_mutation(self):
+        response = self.query(
+            '''
+            mutation {
+                updateExperiment(updateExperimentData:
+                    {
+                    name:"Test Experiment Update",
+                    description: "This is a test experiment update",
+                    id:"6438f6ea0b7e9d05970def5e"
+                    }
+                ) {
+                    experiment{
+                    id
+                    name
+                    description
+                    dynamicVars
+                    createdAt
+                    updatedAt
+                  }
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+        print('Content data--------', content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+
+        # Add some more asserts if you like
+        
+    def test_update_experiment_with_incorrect_id(self):
+        response = self.query(
+            '''
+            mutation {
+                updateExperiment(updateExperimentData:
+                    {
+                    name:"Test Experiment Update",
+                    description: "This is a test experiment update",
+                    id:"6438f6ea0b7e9d05970def5b"
+                    }
+                ) {
+                    experiment{
+                    id
+                    name
+                    description
+                    dynamicVars
+                    createdAt
+                    updatedAt
+                  }
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)                
+
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        # Add some more asserts if you like
+
+    
+    
+
+
+      
---
+++ graphQL/test/testcases_test.py
@@ -0,0 +1,141 @@
+import json
+from graphene_django.utils.testing import GraphQLTestCase
+
+# Command to run test cases for experiment
+# $ python manage.py test graphQL.test.testcases_test
+# Create your tests here.
+class TestcasesTest(GraphQLTestCase):
+    def test_get_testcases_query(self):
+        response = self.query(
+            '''
+            query 
+                {
+                    testCases(experimentId: "6438f6ea0b7e9d05970def5a") {
+                        id
+                        name
+                        description
+                        dynamicVarValues {
+                        key
+                        value
+                        }
+                        experimentId
+                        expectedResult
+                        updatedAt
+                        createdAt
+                    }
+                }
+            '''      
+          )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+      
+        # Add some more asserts if you like
+      
+    def test_create_testcase_mutation(self):
+        response = self.query(
+            '''
+            mutation {
+                createTestCases(testCaseData:{
+                    experimentId:"6438f6ea0b7e9d05970def5a",
+                    name:"new Testcase",
+                    dynamicVarValues:{key:"hey",value:"value"},
+                    expectedResult:["hey","hey10"]}
+                ) {
+                    testCase {
+                    id
+                    name
+                    description
+                    expectedResult
+                    dynamicVarValues {
+                        key
+                        value
+                    }
+                    createdAt
+                    updatedAt
+                    experimentId
+                    }
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+        print('################################################\n')
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+        self.assertEqual(content['data']['createTestCases']['testCase']['name'], 'new Testcase')
+
+    
+    def test_create_testcase_mocked_mutations(self):
+        response = self.query(
+            '''
+            mutation {
+                createTestCases(testCaseData:{
+                    name:"new Testcase",
+                    dynamicVarValues:{key:"hey",value:"value"},
+                    expectedResult:["hey","hey10"]}
+                ) {
+                    testCase {
+                    id
+                    name
+                    description
+                    expectedResult
+                    dynamicVarValues {
+                        key
+                        value
+                    }
+                    createdAt
+                    updatedAt
+                    experimentId
+                    }
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        self.assertEqual(content['errors'][0]['message'], "Field 'TestCaseInput.experimentId' of required type 'ID!' was not provided.")
+        
+    
+    def test_create_testcase_invalid_params(self):
+        response = self.query(
+            '''
+            mutation {
+                createTestCases(testCaseData:{
+                    experimentId:"6438f6ea0b7e9d05970oef5a",
+                    name:"new Testcase",
+                    dynamicVarValues:{key:"hey",value:"value"},
+                    expectedResult:["hey","hey10"]}
+                ) {
+                    testCase {
+                    id
+                    name
+                    description
+                    expectedResult
+                    dynamicVarValues {
+                        key
+                        value
+                    }
+                    createdAt
+                    updatedAt
+                    experimentId
+                    }
+                }
+            }
+            '''
+        )
+
+        content = json.loads(response.content)
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        self.assertEqual(content['errors'][0]['message'], 'Something went wrong')
+        self.assertEqual(content['errors'][0]['extensions']['code'],'m_b_1')
+        self.assertEqual(content['errors'][0]['extensions']['debug'],'Something went wrong')
+        self.assertEqual(content['data']['createTestCases'],None)
+
+        
