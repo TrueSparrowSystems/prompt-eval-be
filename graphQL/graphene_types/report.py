c2d428171d9c926efc52ebf606c773a5c62b1a8e
Yagnik Thummar <64009176+yagnik159@users.noreply.github.com>
2023-04-20 18:30:03 +0530

    get Report and dbml changes (#48)

--- graphQL/graphene_types/report.py
+++ graphQL/graphene_types/report.py
@@ -1,5 +1,5 @@
 from graphene import relay,String,Int, List, ObjectType, ID, Float, InputObjectType
-from graphQL.graphene_types.test_case_evaluation_result import TestCasesEvaluationsResult
+from graphQL.graphene_types.evaluation_test_case_relation import EvaluationTestCaseRelationType
 
 class ReportType(ObjectType):
     id = ID()
@@ -7,10 +7,10 @@ class ReportType(ObjectType):
     eval = String()
     accuracy = Float()
     prompt_template_id = ID()
-    evaluation_report_data = String()
     run_id = Int()
     status = String()
+    initiated_at = Int()
     completed_at = Int()
     created_at = Int()
     updated_at = Int()
-    test_case_evaluation_report = List(TestCasesEvaluationsResult)
+    test_case_evaluation_report = List(EvaluationTestCaseRelationType)
