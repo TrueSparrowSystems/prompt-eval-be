558a9efbc990a05b62b9f68fb1e9a6ef6ecba8f0
Shraddha Falane <falane.shraddha18@gmail.com>
2023-04-24 10:15:42 +0530

    Minor changes

--- api/settings.py
+++ api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('PE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['prompt-eval.quick-poc.com', 'localhost']
+ALLOWED_HOSTS = ['prompt-eval.quick-poc.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
