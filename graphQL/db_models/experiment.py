2e086029b406f84dad8cff79629e14e04c41dcb1
Yagnik Thummar <yagnikthummar0204@gmail.com>
2023-04-25 15:55:20 +0530

    dynamic vars changes

--- graphQL/db_models/experiment.py
+++ graphQL/db_models/experiment.py
@@ -1,3 +1,4 @@
+import json
 from mongoengine.fields import (
     StringField,
     ListField,
@@ -10,3 +11,13 @@ class Experiment(ModelBase):
     name = StringField(required=True)
     description = StringField(max_length=255)
     dynamic_vars = ListField(StringField(max_length=255))
+    
+    @classmethod
+    def update_dynamic_vars(cls, experiment_id, dynamic_vars_dict):
+
+        experiment = cls.objects(id=experiment_id).first()
+        if not experiment:
+            raise ValueError('Experiment not found')
+        experiment.dynamic_vars = list(dynamic_vars_dict.keys())
+        experiment.save()
+
