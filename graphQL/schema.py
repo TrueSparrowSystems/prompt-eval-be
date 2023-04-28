1ba4a4625d4281c612bede133ca52648bec0cdb7
Yagnik Thummar <yagnikthummar0204@gmail.com>
2023-04-28 15:35:28 +0530

    Update TestCases

--- graphQL/schema.py
+++ graphQL/schema.py
@@ -5,6 +5,7 @@ from graphQL.graphene_mutations.create_prompt_template import CreatePromptTempla
 from graphQL.graphene_mutations.update_prompt_template import UpdatePromptTemplateMutation
 from graphQL.graphene_mutations.create_evaluation import CreateEvaluationMutation
 from graphQL.graphene_mutations.create_test_cases import CreateTestCasesMutation
+from graphQL.graphene_mutations.update_test_cases import UpdateTestCasesMutation
 from graphQL.db_models.experiment import Experiment
 from graphQL.db_models.prompt_template import PromptTemplate
 from graphQL.db_models.test_case import TestCase
@@ -22,6 +23,7 @@ class Mutations(graphene.ObjectType):
     create_prompt_template = CreatePromptTemplateMutation.Field()
     update_prompt_template = UpdatePromptTemplateMutation.Field()
     create_test_cases = CreateTestCasesMutation.Field()
+    update_test_cases = UpdateTestCasesMutation.Field()
     create_evaluation = CreateEvaluationMutation.Field()
 
     
--- graphQL/test/test_testcase.py
+++ graphQL/test/test_testcase.py
@@ -88,7 +88,7 @@ class TestcasesTest(GraphQLTestCase):
 
         content = json.loads(response.content)
         print('################################################\n')
-        # TestcasesTest.testcaseID = content['data']['createTestCases']['testCase']['id']
+        TestcasesTest.testcaseID = content['data']['createTestCases']['testCase']['id']
         # This validates the status code and if you get errors
         self.assertResponseNoErrors(response)
         self.assertEqual(content['data']['createTestCases']['testCase']['name'], 'new Testcase')
@@ -123,3 +123,64 @@ class TestcasesTest(GraphQLTestCase):
         # This validates the status code and if you get errors
         self.assertResponseHasErrors(response)
         self.assertEqual(content['errors'][0]['message'], "Field 'TestCaseInput.experimentId' of required type 'ID!' was not provided.")
+    
+    def test_update_testcase_mutation(self):
+        variable = {"id": TestcasesTest.testcaseID, "dynamicVarValues": json.dumps({"key":"hey","value":"value"})}
+        response = self.query(
+            '''
+            mutation updateTestCase($id: String!, $dynamicVarValues: JSONString) {
+                updateTestCases(updateTestCaseData:{
+                    id:$id,
+                    name:"new Testcase",
+                    dynamicVarValues:$dynamicVarValues,
+                    expectedResult:["hey","hey10"]}
+                ) {
+                    testCase {
+                    id
+                    name
+                    description
+                    expectedResult
+                    dynamicVarValues
+                    createdAt
+                    updatedAt
+                    experimentId
+                    }
+                }
+            }
+            ''', variables=variable
+        )
+
+        content = json.loads(response.content)
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+        self.assertEqual(content['data']['updateTestCases']['testCase']['name'], 'new Testcase')
+
+    def test_update_testcase_mocked_mutations(self):
+        variable = {"id": TestcasesTest.testcaseID, "dynamicVarValues": json.dumps({"key":"hey","value":"value"})}
+        response = self.query(
+            '''
+            mutation updateTestCase($id: String!, $dynamicVarValues: JSONString) {
+                updateTestCases(updateTestCaseData:{
+                    name:"new Testcase",
+                    dynamicVarValues:$dynamicVarValues,
+                    expectedResult:["hey","hey10"]}
+                ) {
+                    testCase {
+                    id
+                    name
+                    description
+                    expectedResult
+                    dynamicVarValues
+                    createdAt
+                    updatedAt
+                    experimentId
+                    }
+                }
+            }
+            ''', variables=variable
+        )
+
+        content = json.loads(response.content)
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        self.assertEqual(content['errors'][0]['message'], "Field 'UpdateTestCaseInput.id' of required type 'String!' was not provided.")
