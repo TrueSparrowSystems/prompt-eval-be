ed267b4564c5cdea48747884b0c8340d861e6846
Shraddha Falane <falane.shraddha18@gmail.com>
2023-05-01 10:46:15 +0530

    minor

--- bg_jobs/executor.py
+++ bg_jobs/executor.py
@@ -1,7 +1,7 @@
 
 import concurrent.futures
 
-#submitted_tasks = []
+submitted_tasks = []
 class SingletonThreadPoolExecutor(concurrent.futures.ThreadPoolExecutor):
     def __new__(cls):
         if not hasattr(cls, '_instance'):
@@ -19,6 +19,25 @@ class SingletonThreadPoolExecutor(concurrent.futures.ThreadPoolExecutor):
         #             args.remove(task)
         #     print('task array in else block   :', args)
         #     print('submitted_task array in else block   :', submitted_tasks)
+        # submitted_tasks.extend(args)
         return super().map(fn, *args)
+    
+    # def submit(self, fn, *args, **kwargs):
+    #     print('In execute method    :',args)  
+    #     if len(submitted_tasks) == 0:
+    #         print('task array in if block   :', args)
+    #         print('submitted_task array in if block   :', submitted_tasks)
+    #         submitted_tasks.append(args)
+    #     else:
+    #         for task in submitted_tasks:
+    #             if args.index(task):
+    #                 #args.remove(task)
+    #                 return
+    #         submitted_tasks.append(args)
+    #         print('task array in else block   :', args)
+    #         print('submitted_task array in else block   :', submitted_tasks)
+         
+    #     print('In submit method')
+    #     return super().submit(fn, *args, **kwargs)
 
 
