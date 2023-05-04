21e95b27c198e53277331197222c7e2014a85a9d
Shraddha Falane <falane.shraddha18@gmail.com>
2023-05-04 18:01:47 +0530

    minor changes

--- bg_jobs/globals.py
+++ bg_jobs/globals.py
@@ -6,6 +6,5 @@ EVALS_CLASS_DICT = {
 "test-match": "evals.elsuite.basic.match:Match",
 "coqa-match": "evals.elsuite.basic.match:Match",
 "regex-match": "evals.elsuite.basic.match:Match",
-"fuzzy_match": "evals.elsuite.basic.fuzzy_match:FuzzyMatch",
 "graphql-fuzzy": "evals.elsuite.graphql:GraphQL"
 }
