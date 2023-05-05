0dab3a0d835305be416d11f949e11a406cf40f60
Yagnik Thummar <64009176+yagnik159@users.noreply.github.com>
2023-05-05 15:53:30 +0530

    Sequence diagram (#78)
    Sequence diagram

--- README.md
+++ README.md
@@ -7,58 +7,62 @@ Follow the instructions below for installation
 1. Check you have installed python version greater than or equal to 3.9
 2. Create a python virtual environment
 
-```
+```sh { language=sh }
 python3 -m venv .venv
 ```
 
 3. Activate the virtual environment
 
-```
+```sh { language=sh }
 source .venv/bin/activate
 ```
 
 3. Upgrade to the latest pip version
 
-```
+```sh { language=sh }
 python -m pip install --upgrade pip
 ```
 
 4. Install the dependencies
 
-```
+```sh { language=sh }
 pip install -r requirements.txt
 ```
 
 5. Copy the contents of .env.sample file in .env file and add the values for all env variables.
-
 6. Clone the [evals](https://github.com/openai/evals) submodule
 
-```
+```sql { language=sql }
 git submodule init
 git submodule update
 ```
 
 7. Install submodule dependencies
 
-```
+```sh { language=sh }
 pip install -e evals_framework
 ```
 
 8. Run the api server using following command
 
-```
+```sh { language=sh }
 python manage.py runserver 8000
 ```
 
 9. Run the test cases using following command
 
-```
+```sh { language=sh }
 python manage.py test graphQL
 ```
 
 10. Run the following command to generate test coverage report
 
-```
+```sh { language=sh }
 coverage run manage.py test
 coverage report
 ```
+
+## Documents
+
+1. Sequence diagrams: - docs/ sequenceDiagram.mermaid
+2. DBML diagrams: - docs/ db.dbml
---
+++ graphQL/test/test_report.py
@@ -0,0 +1,230 @@
+import json
+from graphene_django.utils.testing import GraphQLTestCase
+from graphQL.db_models.experiment import Experiment
+from graphQL.db_models.prompt_template import PromptTemplate
+from graphQL.db_models.evaluation import Evaluation
+
+# Command to run test cases for experiment
+# $ python manage.py test graphQL.test.test_report
+# Create your tests here.
+
+class ReportTest(GraphQLTestCase):
+    def test_get_report_query(self):
+        experiment = Experiment.objects.create(name="Test Experiment", description="This is a test experiment")
+        prompt = PromptTemplate.objects.create(experiment_id=str(experiment.id),name="Test Prompt", description="This is a test prompt")
+        evaluation = Evaluation.objects.create(prompt_template_id=str(prompt.id),model="Test Model",eval="Test Eval")
+        variables = {"reportID": str(evaluation.id)}
+        response = self.query(
+            '''
+            query getReport($reportID:String!){
+                getReport(reportId:$reportID,page:1,limit:2){
+                    id
+                    model
+                    eval
+                    accuracy
+                    promptTemplateId
+                    runId
+                    status
+                    initiatedAt
+                    completedAt
+                    createdAt
+                    updatedAt
+                    testCaseEvaluationReport{
+                        id
+                        prompt{
+                            role,
+                            content
+                        }
+                        testCaseId
+                        testCaseName
+                        testCaseDescription
+                        actualResult
+                        acceptableResult
+                        accuracy
+                    }
+                }
+                }
+            '''  , variables=variables    
+          )
+
+        content = json.loads(response.content)
+        print('----------------------content----------------------',content)
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+      
+        # Add some more asserts if you like
+    def test_get_report_query_with_invalid_id(self):
+        response = self.query(
+            '''
+            query {
+                getReport(reportId:1123,page:1,limit:2){
+                    id
+                    model
+                    eval
+                    accuracy
+                    promptTemplateId
+                    runId
+                    status
+                    initiatedAt
+                    completedAt
+                    createdAt
+                    updatedAt
+                    testCaseEvaluationReport{
+                        id
+                        prompt{
+                            role,
+                            content
+                        }
+                        testCaseId
+                        testCaseName
+                        testCaseDescription
+                        actualResult
+                        acceptableResult
+                        accuracy
+                    }
+                }
+                }
+            '''      
+          )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        self.assertEqual(content['errors'][0]['message'], 'String cannot represent a non string value: 1123')
+      
+        # Add some more asserts if you like
+    
+    def test_get_report_query_with_invalid_page(self):
+        response = self.query(
+            '''
+            query {
+                getReport(reportId:"64410b5c8402313be9d1f85e",page:-1,limit:2){
+                    id
+                    model
+                    eval
+                    accuracy
+                    promptTemplateId
+                    runId
+                    status
+                    initiatedAt
+                    completedAt
+                    createdAt
+                    updatedAt
+                    testCaseEvaluationReport{
+                        id
+                        prompt{
+                            role,
+                            content
+                        }
+                        testCaseId
+                        testCaseName
+                        testCaseDescription
+                        actualResult
+                        acceptableResult
+                        accuracy
+                    }
+                }
+                }
+            '''      
+          )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        self.assertEqual(content['errors'][0]['message'], 'Something went wrong')
+        self.assertEqual(content['errors'][0]['extensions']['code'], 'g_s_q_r_4')
+        self.assertEqual(content['errors'][0]['extensions']['debug'], 'Something_went_wrong')
+        self.assertEqual(content['data']['getReport'], None)
+        # Add some more asserts if you like
+
+    def test_get_report_query_with_invalid_limit(self):
+        response = self.query(
+            '''
+            query {
+                getReport(reportId:"64410b5c8402313be9d1f85e",page:1,limit:-1){
+                    id
+                    model
+                    eval
+                    accuracy
+                    promptTemplateId
+                    runId
+                    status
+                    initiatedAt
+                    completedAt
+                    createdAt
+                    updatedAt
+                    testCaseEvaluationReport{
+                        id
+                        prompt{
+                            role,
+                            content
+                        }
+                        testCaseId
+                        testCaseName
+                        testCaseDescription
+                        actualResult
+                        acceptableResult
+                        accuracy
+                    }
+                }
+                }
+            '''      
+          )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        self.assertEqual(content['errors'][0]['message'], 'Something went wrong')
+        self.assertEqual(content['errors'][0]['extensions']['code'], 'g_s_q_r_4')
+        self.assertEqual(content['errors'][0]['extensions']['debug'], 'Something_went_wrong')
+        self.assertEqual(content['data']['getReport'], None)
+      
+        # Add some more asserts if you like
+
+    def test_get_report_query_with_invalid_page_and_limit(self):
+        response = self.query(
+            '''
+            query {
+                getReport(reportId:"64410b5c8402313be9d1f85e",page:-1,limit:-1){
+                    id
+                    model
+                    eval
+                    accuracy
+                    promptTemplateId
+                    runId
+                    status
+                    initiatedAt
+                    completedAt
+                    createdAt
+                    updatedAt
+                    testCaseEvaluationReport{
+                        id
+                        prompt{
+                            role,
+                            content
+                        }
+                        testCaseId
+                        testCaseName
+                        testCaseDescription
+                        actualResult
+                        acceptableResult
+                        accuracy
+                    }
+                }
+                }
+            '''      
+          )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseHasErrors(response)
+        self.assertEqual(content['errors'][0]['message'], 'Something went wrong')
+        self.assertEqual(content['errors'][0]['extensions']['code'], 'g_s_q_r_4')
+        self.assertEqual(content['errors'][0]['extensions']['debug'], 'Something_went_wrong')
+        self.assertEqual(content['data']['getReport'], None)
+      
+        # Add some more asserts if you like
