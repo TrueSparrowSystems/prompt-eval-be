0e9b321d20128707593c89b4e386c08056752a0a
Raj Shah <rajshah0211@gmail.com>
2023-06-23 15:53:16 +0530

    Updated comment

--- bg_jobs/background_job.py
+++ bg_jobs/background_job.py
@@ -2,29 +2,28 @@ from bg_jobs.background.bg_job import BgJob
 import bg_jobs.globals as globals
 import time
 
-def background_job(params):
-    """
-    Function to run a background job with given parameters.
-    @params: Dictionary containing the parameters for the background job.
-    @return: The result of the background job execution or an exception if an error occurs.
-    """
-    
-    # Print the value of the global variable SIGINT_TRIGGERED
-    print('sigint_triggered:::', globals.SIGINT_TRIGGERED)
+"""
+Function to run a background job with given parameters.
 
-    # Check if SIGINT_TRIGGERED is False
+@param {object} params
+@param {string} params.evaluation_id
+@param {string} params.prompt_template_id
+
+@returns: Returns the error message if any, else None.
+"""
+def background_job(params):
+    
+    print('Sigint Triggered:::', globals.SIGINT_TRIGGERED)
+
     if not globals.SIGINT_TRIGGERED:
         globals.PROCESS_COMPLETED = False
         try:
-            # Print a message indicating the start of the background job with the given params
             print(f"**********Background job started with given params {params['evaluation_id']}, {params['prompt_template_id']}**********")
-            # Create an instance of the BgJob class and perform the task
             task = BgJob(params)
             return task.perform()
         except Exception as e:
             return e
         finally:
-            # Set the global variable PROCESS_COMPLETED to True
             globals.PROCESS_COMPLETED = True
             print("Process Completed::: ", globals.PROCESS_COMPLETED)
             
